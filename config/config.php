7b8a7eb81e6c536bd4aa4c025240f4c2d2bd1ce7
itsrd769 <97438592+itsrd769@users.noreply.github.com>
2022-06-26 14:01:57 +0530

    Update config.php

--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@ ob_start();
 session_start();
 
 $timezone = date_default_timezone_set("Asia/Dhaka");
-$con = mysqli_connect("localhost", "root", "", "virtualclassroom");
+$con = mysqli_connect("sql6.freemysqlhosting.net", "sql6502208", "FUzmpqeMK3", "sql6502208");
 
 if(mysqli_connect_errno())
 {
