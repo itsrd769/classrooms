52d6ab3e99ac149e9abd3769022fe9c70806f2fc
itsrd769 <97438592+itsrd769@users.noreply.github.com>
2022-07-15 17:13:24 +0530

    Delete includes directory

--- includes/form_handlers/search.php
+++
@@ -1,8 +0,0 @@
-<?php 
-require '../../config/config.php';
-if (isset($_GET['classCode'])) {
-    $courseCode = $_GET['classCode'];
-    $search = "asdas";
-	header("Location: ../../classRoom.php?classCode=$courseCode&search=$search");
-}
-?>
